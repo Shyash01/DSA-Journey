66a08a49e97063e93b91f8a12e7d5e5946cc2867
Yash <shyash2000@gmail.com>
2026-07-22 08:06:21 +0530

    Auto update

--- array2/reverse_without_extra_array.cpp
+++ array2/reverse_without_extra_array.cpp
@@ -1,5 +1,8 @@
+// #include<bits/stdc++.h>
 #include<iostream>
 #include<vector>
+#include<algorithm>
+
 
 using namespace std;
 
@@ -10,21 +13,44 @@ void display(vector <int>&a){
     }
     cout<<endl;
 }
+
+void reversePart(int i, int j,vector<int> & a){
+    while(i<=j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
+        i++;
+        j--;
+    }
+
+}
 int main(){
     vector<int> v1 = {1,3,2,4,3,4,1,6};
 
     display(v1);
 
-    int i=0;
-    int j=v1.size()-1;
+    // int i=0;
+    // int j=v1.size()-1;
 
-    while(i<j){
-        int temp = v1[i];
-        v1[i] = v1[j];
-        v1[j] = temp;
-        i++;
-        j--;
-    }
+    // while(i<j){
+    //     int temp = v1[i];
+    //     v1[i] = v1[j];
+    //     v1[j] = temp;
+    //     i++;
+    //     j--;
+    // }
+
+    // reverse(v1.begin(),v1.end()); // In built function
+    int i;
+    cout<<"Enter first index: ";
+    cin>>i;
+
+    int j;
+    cout<<"Enter last index: ";
+    cin>>j;
+
+    reversePart(i,j,v1);
 
     display(v1);
 }
