55445160828d90bcace98ded93597e8eb6a9bc3d
Yash <shyash2000@gmail.com>
2026-07-25 00:38:51 +0530

    Auto update

---
+++ array3/tempCodeRunnerFile.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+        int idx = -1;
+        int n = nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        
+        cout<<idx<<endl;
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            reverse(nums.begin()+idx,nums.end());
+            for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+            int temp = nums[idx];
+            nums[idx]=nums[idx+1];
+            nums[idx+1] = temp;
+            
+        }   
+        
+      
+
+
+    }
+int main(){
+    vector<int> nums = {1,1,5};
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+    nextPermutation(nums);
+
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+}
