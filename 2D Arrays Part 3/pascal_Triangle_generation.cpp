65899773a6179c4180a2afea860f875b18e081c7
Yash <shyash2000@gmail.com>
2026-08-05 18:01:25 +0530

    Explore Concepts Of MultiDimensional Arrays

--- 2D Arrays Part 3/pascal_Triangle_generation.cpp
+++ 2D Arrays Part 3/pascal_Triangle_generation.cpp
@@ -6,12 +6,28 @@ int main(){
     int m = 7;
     vector< vector<int> > v;
 
-    for(int i=1;i<=m;i++){
-        vector<int> a(i);
-        v.push_back(a);
-    }
+    // for(int i=1;i<=m;i++){
+    //     vector<int> a(i);
+    //     v.push_back(a);
+    // }
+
+    // for(int i=0;i<m;i++){
+    //     for(int j=0;j<v[i].size();j++){
+    //         if(j==0 || j==i){
+    //             v[i][j] = 1;
+
+    //         }
+    //         else{
+    //             v[i][j] = v[i-1][j] + v[i-1][j-1];
+    //         }
+    //     }
+    // }
+
+    // Combining Both The Loops
 
     for(int i=0;i<m;i++){
+        vector<int> a(i+1);
+        v.push_back(a);
         for(int j=0;j<v[i].size();j++){
             if(j==0 || j==i){
                 v[i][j] = 1;
@@ -22,7 +38,6 @@ int main(){
             }
         }
     }
-
 
     for(int i=0;i<m;i++){
         for(int j=0;j<v[i].size();j++){
---
+++ DSA_Coding Practice/Decode++_Problems/Array Problems/findLargestThirdnumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<climits>
+#include<vector>
+
+int main(){
+    int largest = INT_MIN;
+    int sec_lar = INT_MIN;
+    int third_lar = INT_MIN;
+
+    vector<int> arr = {8,2,4,1,5,7,0,9,3,6};
+    
+
+
+
+}
