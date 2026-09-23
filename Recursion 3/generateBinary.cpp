489f827f1566febafac3be4cf6eb9f3f176f6143
Yash <shyash2000@gmail.com>
2026-09-23 22:24:40 +0530

    Auto update

---
+++ Recursion 3/generateBinary.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int n =3;
+
+void generate(string s){
+    if(s.length()==n){
+        cout<<s<<endl;
+        return;
+    } 
+
+    generate(s+"0");
+    generate(s+"1");
+}
+int main(){
+    int n;
+    cin>>n;
+    
+    generate("");
+}
